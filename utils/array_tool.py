0c0486ca3942b00286501935b140d1185304c0e1
shkim960520 <shkim960520@gmail.com>
2021-04-15 16:20:10 +0900

    update data dir and utils dir

---
+++ utils/array_tool.py
@@ -0,0 +1,24 @@
+import torch
+import numpy as np
+
+def tonumpy(data):
+    if isinstance(data, np.ndarray):
+        return data
+    if isinstance(data, torch.Tensor):
+        return data.detach().cpu().numpy()
+
+
+def totensor(data, cuda = True):
+    if isinstance(data, np.ndarray):
+        tensor = torch.from_numpy(data)
+    if isinstance(data, torch.Tensor):
+        tnesor = data.detach()
+    if cuda:
+        tensor = tensor.cuda()
+    return tensor
+
+def scalar(data):
+    if isinstance(data, np.ndarray):
+        return data.reshape(1)[0]
+    if isinstance(data, torch.Tensor):
+        return data.item()
